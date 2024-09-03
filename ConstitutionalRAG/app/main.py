940ef61945ed6b2519062f6cff95e2d9057cc1bd
hereisSwapnil <swapnilskumars@gmail.com>
2024-09-03 21:58:00 +0530

    added language translation

--- ConstitutionalRAG/app/main.py
+++ ConstitutionalRAG/app/main.py
@@ -1,3 +1,6 @@
+from dotenv import load_dotenv
+import os
+from transformers import M2M100ForConditionalGeneration, M2M100Tokenizer
 from fastapi import FastAPI, HTTPException
 from app.config import Config
 from app.utils.loader import create_prompt_template
@@ -15,6 +18,29 @@ prompt_template = create_prompt_template()
 
 # Create vector embeddings
 vectors = create_embeddings('./data')
+
+# Load environment variables
+load_dotenv()
+
+# Get API key from environment variables (not needed if using local model)
+api_key = os.getenv("HUGGINGFACE_API_KEY")
+
+# Define the repository ID
+repo_id = "facebook/m2m100_418M"
+
+# Initialize the model and tokenizer
+model = M2M100ForConditionalGeneration.from_pretrained(repo_id)
+tokenizer = M2M100Tokenizer.from_pretrained(repo_id)
+
+
+def translate_text(text, target_lang):
+    tokenizer.src_lang = "en"
+    encoded_text = tokenizer(text, return_tensors="pt")
+    generated_tokens = model.generate(
+        **encoded_text, forced_bos_token_id=tokenizer.get_lang_id(target_lang))
+    translated_text = tokenizer.batch_decode(
+        generated_tokens, skip_special_tokens=True)
+    return translated_text
 
 
 @app.post("/get_response/")
